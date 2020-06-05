bedea5e78ae14c6a67f5b3d4c1973ae55728a112
Iheb Haboubi <iheb.haboubi56@gmail.com>
2020-06-05 20:12:00 +0100

    Refactor code

--- run.py
+++ run.py
@@ -5,15 +5,16 @@ import random
 import colorama
 from termcolor import colored
 
-#this is necessary in order to display colors in windows
+# this is necessary in order to display colors in windows
 colorama.init()
 
 GRID = [
     [None, None, None],
     [None, None, None],
-    [None, None, None]]
+    [None, None, None],
+]
 
-#indexes of rows,coloumns and diagonals
+# indexes of rows,coloumns and diagonals
 ELEMENTS = [
     [(0, 0), (0, 1), (0, 2)],
     [(0, 0), (1, 0), (2, 0)],
@@ -22,15 +23,19 @@ ELEMENTS = [
     [(2, 0), (2, 1), (2, 2)],
     [(0, 2), (1, 2), (2, 2)],
     [(0, 0), (1, 1), (2, 2)],
-    [(0, 2), (1, 1), (2, 0)]]
+    [(0, 2), (1, 1), (2, 0)],
+]
 
+INFINITY = float("inf")
 CLEAR = "clear" if os.name == "posix" else "cls"
+
 
 def shuffled(iter):
     # return a randomly shuffled version of an interable
     iter = list(iter)
-    random.shuffle(iter)
+    random.shuffle(iter)    
     return iter
+
 
 def similar(x, indexes):
     # check if elements of a row / coloumn / diagonal are the same
@@ -42,14 +47,16 @@ def similar(x, indexes):
             return False
     return True
 
+
 def color(x, indexes):
-    #if elements of a row / coloumn / diagonal are the same, color them in red
+    # if elements of a row / coloumn / diagonal are the same, color them in red
     for i, j in indexes:
         x[3 * i + j] = colored(x[3 * i + j], "red")
 
+
 def display(grid):
     x = [grid[i][j] or " " for i in range(3) for j in range(3)]
-    #if the computer wins color the winning row / coloumn / diagonal
+    # if the computer wins color the winning row / coloumn / diagonal
     for element in ELEMENTS:
         if similar(x, element):
             color(x, element)
@@ -61,14 +68,17 @@ def display(grid):
         ---+---+---                     ---+---+---
          7 | 8 | 9                       {} | {} | {}
     
-        Choose a number : """.format(*x)
+        Choose a number : """.format(
+        *x
+    )
 
-    #clear the screen to print at the same place
+    # clear the screen to print at the same place
     os.system(CLEAR)
     print(result, end="")
 
+
 def win_player(grid, char):
-    #check if a player wins the game
+    # check if a player wins the game
     # check rows
     for i in range(3):
         if all(grid[i][j] == char for j in range(3)):
@@ -83,16 +93,18 @@ def win_player(grid, char):
     if all(grid[i][2 - i] == char for i in range(3)):
         return True
 
+
 def coordinates(choice):
-    #return the row and coloumn corresponding to user input
+    # return the row and coloumn corresponding to user input
     # get row
     row = 3 - ((choice + 2) // 3)
     # get col
     col = (choice - 1) % 3
     return row, col
 
+
 def terminal(grid):
-    #check if the game is at a terminal state
+    # check if the game is at a terminal state
     # player wins
     if win_player(grid, "X"):
         return True
@@ -107,7 +119,7 @@ def terminal(grid):
 
 
 def utility(grid):
-    #return the score corresponding to the terminal state
+    # return the score corresponding to the terminal state
     if win_player(grid, "X"):
         return -1
     if win_player(grid, "O"):
@@ -116,7 +128,7 @@ def utility(grid):
 
 
 def actions(grid):
-    #return possible actions a player can take at each state
+    # return possible actions a player can take at each state
     result = []
     for i in shuffled(range(3)):
         for j in shuffled(range(3)):
@@ -126,17 +138,17 @@ def actions(grid):
 
 
 def minimax(grid, computer, alpha, beta):
-    #return the maximum value a player can obtain at each step
+    # return the maximum value a player can obtain at each step
     if terminal(grid):
         return utility(grid)
 
     if computer:
         func = max
-        m = -float("inf")
+        m = -INFINITY
         char = "O"
     else:
         func = min
-        m = float("inf")
+        m = INFINITY
         char = "X"
 
     for action in actions(grid):
@@ -160,9 +172,8 @@ def minimax(grid, computer, alpha, beta):
 
 def best_move(grid):
     # find all empty cells and compute the minimax for each one
-    alpha = -float("inf")
-    beta = float("inf")
-    m = -float("inf")
+    m = alpha = -INFINITY
+    beta = INFINITY
     for action in actions(grid):
         i, j = action
         grid[i][j] = "O"
